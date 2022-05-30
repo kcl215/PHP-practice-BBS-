9bcc3534abba67a135d8ab6f51d009824d2dae3b
kcl215 <98423401+kcl215@users.noreply.github.com>
2022-05-30 13:38:55 +0900

    section5-85 finish

--- join/index.php
+++ join/index.php
@@ -16,6 +16,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     if ($form['name'] === '') {
         $error['name'] = 'blank';
     }
+    $form['email'] = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    if ($form['email'] === '') {
+        $error['email'] = 'blank';
+    }
+    $form['password'] = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+    if ($form['password'] === '') {
+        $error['password'] = 'blank';
+    } else if (strlen($form['password']) < 4) {
+        $error['password'] = 'length';
+    }
 
 }
 ?>
@@ -50,14 +60,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
                 </dd>
                 <dt>メールアドレス<span class="required">必須</span></dt>
                 <dd>
-                    <input type="text" name="email" size="35" maxlength="255" value=""/>
-                    <p class="error">* メールアドレスを入力してください</p>
+                    <input type="text" name="email" size="35" maxlength="255" value="<?php echo h($form['email']) ?>"/>
+                    <?php if (isset($error['email']) && $error['email'] === 'blank'): ?>
+                        <p class="error">* メールアドレスを入力してください</p>
+                    <?php endif; ?>
                     <p class="error">* 指定されたメールアドレスはすでに登録されています</p>
                 <dt>パスワード<span class="required">必須</span></dt>
                 <dd>
-                    <input type="password" name="password" size="10" maxlength="20" value=""/>
-                    <p class="error">* パスワードを入力してください</p>
-                    <p class="error">* パスワードは4文字以上で入力してください</p>
+                    <input type="password" name="password" size="10" maxlength="20" value="<?php echo h($form['password']) ?>"/>
+                    <?php if (isset($error['password']) && $error['password'] === 'blank'): ?>
+                        <p class="error">* パスワードを入力してください</p>
+                    <?php endif; ?>
+                    <?php if (isset($error['password']) && $error['password'] === 'length'): ?>
+                        <p class="error">* パスワードは4文字以上で入力してください</p>
+                    <?php endif; ?>
                 </dd>
                 <dt>写真など</dt>
                 <dd>
