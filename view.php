0c25fa3b0eb507074717410f510ae579f3baafb2
kcl215 <98423401+kcl215@users.noreply.github.com>
2022-06-01 18:59:29 +0900

    section5-100 finish

--- view.php
+++ view.php
@@ -57,8 +57,11 @@ $db = dbconnect();
                         <img src="member_picture/<?php echo h($picture); ?>" width="48" height="48" alt="" />
                     <?php endif; ?>
                     <p><?php echo h($message); ?> <span class="name">（<?php echo h($name); ?>）</span></p>
-                    <p class="day"><a href="view.php?id="><?php echo h($created); ?></a>
-                        [<a href="delete.php?id=" style="color: #F33;">削除</a>]
+                    <p class="day">
+                        <a href="view.php?id="><?php echo h($created); ?></a>
+                        <?php if ($_SESSION['id'] === $member_id): ?>
+                            [<a href="delete.php?id=<?php echo h($id); ?>" style="color: #F33;">削除</a>]
+                        <?php endif; ?>
                     </p>
                 </div>
             <?php else : ?>
